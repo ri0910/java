49bca4b321b9eeaaacaf75530d1572cae1813788
Riya karmakar <riyakarmakar.kolkata@gmail.com>
2024-03-04 19:24:44 +0530

    Added new spring projects

---
+++ Spring-Project/JobSecurityWebsite/src/test/java/com/riya/JobSecurityWebsite/JobSecurityWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riya.JobSecurityWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSecurityWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
